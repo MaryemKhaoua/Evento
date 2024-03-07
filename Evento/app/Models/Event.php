97ffc25bdc8c0d775303b244f13e87b779c0f9e5
Maryem Khaoua <maryem.khaoua@gmail.com>
2024-03-07 09:29:00 +0100

    model pour ticket et category

--- Evento/app/Models/Event.php
+++ Evento/app/Models/Event.php
@@ -16,6 +16,21 @@ class Event extends Model
         'description',
         'lieu',
         'date',
-        'media'
+        'acceptation',
+        'media',
+        'user_id',
+        'category_id',
     ];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
+
+    public function tickets(){
+        return $this->hasMany(Ticket::class);
+    }
 }
